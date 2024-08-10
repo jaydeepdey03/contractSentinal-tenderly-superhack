d1da0b64f05b769e8dc8959a7fe7befb9e49b296
Fidal <fidal15perfect@gmail.com>
2024-08-10 16:38:02 +0530

    create-contract update with github api

--- index.js
+++ index.js
@@ -58,12 +58,19 @@ app.get("/runscript", (req, res) => {
 })
 
 // POST route to create a Solidity file
-app.post('/create-contract', (req, res) => {
-    const { fileName, solidityCode } = req.body;
+app.post('/create-contract', async(req, res) => {
+    const { fileName, repoOwner, repoName, filePath, branch } = req.body;
 
     try {
-        if (!fileName || !solidityCode) {
-            return res.status(400).send('fileName and solidityCode are required.');
+        
+        if (!fileName) {
+            return res.status(400).send("No fileName mentioned")
+        }
+        
+        let solidityCode = getContractContent(repoOwner, repoName, filePath, branch)
+
+        if ( !solidityCode) {
+            return res.status(400).send('Unable to fetch Solidity code');
         }
 
         // Define the contracts directory
@@ -85,6 +92,41 @@ app.post('/create-contract', (req, res) => {
 
 
 });
+
+
+const getContractContent = async (repoOwner, repoName, filePath, branch) => {
+  const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
+  const token = process.env.GITHUB_ACCESS_TOKEN;
+
+  // Define headers with the GitHub token
+  const headers = {
+    Authorization: `Bearer ${token}`,
+    Accept: "application/vnd.github.v3+json",
+  };
+
+  try {
+    // Fetch file content from GitHub
+    const response = await axios.get(apiUrl, { headers });
+    
+    // Decode the base64 content
+    const currentContent = Buffer.from(response.data.content, "base64").toString("utf-8");
+    
+    console.log(currentContent, filePath, response.data.sha);
+  } catch (error) {
+    console.error("Error retrieving file content:", error);
+  }
+};
+
+
+// getContractContent()
+// https://github.com/jaydeepdey03/jaydeep-test-alchemy-superhack/blob/main/src/styles/globals.css
+// getContractContent(
+//   "jaydeepdey03",
+//   "jaydeep-test-alchemy-superhack",
+//   "src/styles/globals.css",
+//   "main"
+// );
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
