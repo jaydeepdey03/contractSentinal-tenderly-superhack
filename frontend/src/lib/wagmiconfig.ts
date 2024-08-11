447be3fd4935a3ebc4d0485678107a165d0411ff
jaydeepdey03 <jaydeep.dey03@gmail.com>
2024-08-11 22:11:29 +0530

    deployment

--- frontend/src/lib/wagmiconfig.ts
+++
@@ -1,8 +0,0 @@
-import { http, createConfig } from 'wagmi'
-import { baseSepolia } from 'wagmi/chains'
-export const config = createConfig({
-    chains: [baseSepolia],
-    transports: {
-        [baseSepolia.id]: http(),
-    },
-})
